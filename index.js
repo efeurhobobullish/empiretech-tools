1a5a36b834568dd01fcd84c3503de46bc7cc2df8
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-04-04 03:41:04 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+///====================[ Ai ]============\\
+const { igstorydl } = require('./tools/dowload/igstorydl.js')
+const { twitterdl } = require('./tools/download/twitterdl.js')
+const { tiktokdl } = require('./tools/download/tiktokdl.js')
+const { telesticker } = require('./tools/download/telesticker.js')
+const { ssweb } = require('./tools/download/ssweb.js')
+const { playstore } = require('./tools/download/playstore.ja')
+const { wagroupstalk } = require('./tools/stalker/wagroupstalk.js')
+const { gitstalk } = require('./tools/stalker/gitstalk.ks')
+
+module.exports.igdl= igdl;
+module.exports.fbdl= fbdl;
+module.exports.telesticker = telesticker;
+module.exports.ssweb = ssweb;
+module.exports.playstore= playstore;
+module.exports.wagroupstalk = wagroupstalk;
+module.exports.gitstalk = gitstalk;
+module.exports.igstorydl = igstorydl;
+module.exports.tiktokdl = tiktokdl;
+module.exports.twitterdl =twitterdl;
